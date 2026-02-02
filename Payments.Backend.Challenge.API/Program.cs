da0b57b1381d50bf314f0bbf0141d0a9f64ccf98
Paulo Anjos <pcsar.anjos@gmail.com>
2026-02-01 23:06:21 -0300

    chore: add Program.cs and Docker configuration

--- Payments.Backend.Challenge.API/Program.cs
+++ Payments.Backend.Challenge.API/Program.cs
@@ -1,41 +1,59 @@
+using Microsoft.EntityFrameworkCore;
+using Payments.Backend.Challenge.Application.Interfaces;
+using Payments.Backend.Challenge.Application.UseCases;
+using Payments.Backend.Challenge.Domain.Interfaces;
+using Payments.Backend.Challenge.Domain.Services;
+using Payments.Backend.Challenge.Infrastructure.Persistence;
+using Payments.Backend.Challenge.Infrastructure.Repositories;
+using Payments.Backend.Challenge.Infrastructure.Security;
+using Payments.Backend.Challenge.Infrastructure.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Db context
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite((builder.Configuration.GetConnectionString("DefaultConnection"))));
+
+// Security (password hasher)
+builder.Services.AddScoped<IPasswordHasher, AspNetIdentityPasswordHasher>();
+
+// Repositories
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IWalletRepository, WalletRepository>();
+
+// Domain Services
+builder.Services.AddScoped<PaymentService>();
+builder.Services.AddScoped<UserUniquesPolicy>();
+
+// Use cases
+builder.Services.AddScoped<IPaymentCoordinator, PaymentCoordinator>();
+builder.Services.AddScoped<IRegisterUser, RegisterUser>();
+
+// Unit of work
+builder.Services.AddScoped<IEfUnitOfWork, EfUnitOfWork>();
+
+// Mock services
+builder.Services.AddHttpClient<IExternalAuthorizationMock, ExternalAuthorizationMock>(client =>
+{
+    client.BaseAddress = new Uri("https://util.devi.tools/api/v2/authorize");
+});
+
+builder.Services.AddHttpClient<IExternalNotificationMock, ExternalNotificationMock>(client =>
+{
+    client.BaseAddress = new Uri("https://util.devi.tools/api/v1/notify");
+});
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Auto run migrations on startup
+using (var scope = app.Services.CreateScope())
 {
-    app.MapOpenApi();
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
